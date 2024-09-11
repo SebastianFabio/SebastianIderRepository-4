822989e5008eccf2a903a84e79117c59cb317664
Ider Kawabata <107132431+iderisadev@users.noreply.github.com>
2024-09-11 12:34:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ $(document).ready(function () {
             $(".imag").click(function () { 
                 $(".Outer").toggle("swing"); 
             }); 
+			 $(".imagi").click(function () { 
+                $(".morb").toggle("swing"); 
+            });
+			$(".imagie").click(function () { 
+                $(".harold").toggle("swing"); 
+            }); 			
         }); 
-
