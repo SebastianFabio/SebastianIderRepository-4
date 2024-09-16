3be79b8a2db4d79bc6af4b4b221a3a65da55eabc
SebastianFabio <seabass.fabio@gmail.com>
2024-09-16 13:27:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 $(document).ready(function () { 
             $(".imag").click(function () { 
                 $(".Outer").toggle("swing"); 
-            }); 
+            	}); 
 			 $(".imagi").click(function () { 
                 $(".morb").toggle("swing"); 
-            });
+            	});
 			$(".imagie").click(function () { 
                 $(".harold").toggle("swing"); 
-            });
-			$(".correct").click(function (){
-				$(this).css("color", "green")();
-			});
-			$(".wrong").click(function (){
-				$(this).css("color", "red")();
-			});
+            	});
+		$(".correct").click(function (){
+			$(this).css("color", "green")();
+		});
+		$(".wrong").click(function (){
+			$(this).css("color", "red")();
+		});
         }); 
 		
